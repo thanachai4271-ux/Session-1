22f773a6645aa0e5a6636e519d9851488edae75d
thanachai4271-ux <thanachai4271@gmail.com>
2026-04-10 09:31:23 +0700

    Update task4.py

--- task4.py
+++ task4.py
@@ -6,6 +6,6 @@ m = s.assign(rev=s.quantity*s.price-s.discount_amount.fillna(0)).merge(p[['produ
 t3 = m.groupby('product_name')[['quantity','rev']].sum().nlargest(3,'quantity').reset_index().assign(rev=lambda x: x.rev.apply('${:,.2f}'.format)).set_axis(['product_name','Total Quantity Sold','Total Revenue'], axis=1)
 
 with PdfPages('Session1_ProductPerformancev2.pdf') as d:
-      f, a = plt.subplots(); m[m.category.isin(['Pastries','Bread','Tarte'])].groupby('category')['rev'].sum().plot.bar(color=['#ff9999','#66b3ff','#99ff99'], ax=a, title='Total Revenue by Category', ylabel='Total Revenue ($)', rot=0); d.savefig(f, bbox_inches='tight'); plt.close()
+      f, a = plt.subplots(); m[m.category.isin(['Pastries','Bread','Tarte'])].groupby('category')['rev'].sum().plot.bar(color=['lightpink','lightblue','lightgreen'], ax=a, title='Total Revenue by Category', ylabel='Total Revenue ($)', rot=0); d.savefig(f, bbox_inches='tight'); plt.close()
       f, a = plt.subplots(figsize=(9,3)); a.axis('off'); a.set_title('Top 3 Best-Selling Products', weight='bold'); a.table(cellText=t3.values, colLabels=t3.columns, loc='center', cellLoc='center').scale(1,2)
-      pdf.savefig(f, bbox_inches='tight'); plt.close()
+      d.savefig(f, bbox_inches='tight'); plt.close()
