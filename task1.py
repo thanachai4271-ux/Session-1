30acb64698a71f9496cfe9fd1b94da8c8c195a87
thanachai4271-ux <thanachai4271@gmail.com>
2026-04-15 16:21:07 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -7,12 +7,12 @@ for n, d in v.items():
     r += f"### File: {n}\n\n{d.head().to_string()}\n\nData Types:\n" + "\n".join(f"- {k}: {t}" for k, t in d.dtypes.items()) + "\n\nInconsistencies:\n"
     
     dc = [x for x in d if 'date' in x.lower()]
-    dm = pd.concat([pd.to_datetime(d[x], errors='coerce').dt.year.between(2010,2025)!=True for x in dc], axis=1).any(axis=1) if dc else N
+    dm = pd.concat([pd.to_datetime(d[x], errors='coerce',format='mixed').dt.year.between(2010,2025)!=True for x in dc], axis=1).any(axis=1) if dc else N
     nc = [x for x in d if x in ['quantity','price','cost']]
     nm = pd.concat([pd.to_numeric(S(x).replace(r'\$','',regex=True), errors='coerce')<0 for x in nc], axis=1).any(axis=1) if nc else N
     im = (~d['customer_id'].isin(v['customers.csv']['customer_id']) | ~d['product_id'].isin(v['products.csv']['product_id'])) if c=='s' else N
     um = (~d['gender'].isin(['M','F',pd.NA]) | ~S('membership_status').title().isin(['Basic','Silver','Gold','Nan']) | ~S('churned').title().isin(T)) if c=='c' else (~d['category'].isin(['Pastries','Bread','Tarte','Viennoiserie','Pastry',pd.NA]) | ~S('seasonal').title().isin(T) | ~S('active').title().isin(T)) if c=='p' else (~d['payment_method'].isin(['Credit Card','Mobile Pay','Cash',pd.NA]) | ~d['channel'].isin(['Online','In-store',pd.NA]))
-    fm = (S('first_name').contains(r'^\s|\s$') | S('email').contains(r'[A-Z]') | S('phone_number').contains(r'[a-zA-Z\^<|>@]')) if c=='c' else (S('cost').contains(r'[\(\)]') | S('price').contains(r'[\(\)]')) if c=='p' else N
+    fm = (S('first_name').contains(r'^\s|\s$') | S('email').contains(r'[A-Z]') | S('phone_number').contains(r'[a-zA-Z\^<|>@]')) if c=='c' else (~S('cost').contains(r'\$') | ~S('price').contains(r'\$')) if c=='p' else N
          
     r += f"- Invalid Dates: {sum(dm)}\n- Neg Values: {sum(nm)}\n- Invalid IDs: {sum(im)}\n- Unexpected: {sum(um)}\n- Format Issues: {sum(fm)}\n{'-'*30}\n\n"
 
