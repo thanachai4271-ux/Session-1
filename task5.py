2af6d5d4ed476372ae5de4f73ae71e1f03c171d0
thanachai4271-ux <thanachai4271@gmail.com>
2026-05-01 06:50:59 +0700

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,5 +1,4 @@
-import pandas as pd, matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
+import pandas as pd, matplotlib.pyplot as plt; from matplotlib.backends.backend_pdf import PdfPages
 
 c = pd.read_csv('customers_cleaned.csv')
 
@@ -10,9 +9,9 @@ with PdfPages('Session1_CustomerAnalysis_short.pdf') as pdf:
     g = c[c['gender'].isin(['M','F'])]['gender'].value_counts(normalize=True).mul(100).round(2).reset_index()
     t = c[c['membership_status'].isin(['Basic','Silver','Gold'])].groupby('membership_status')['total_spending'].mean().round(2).reindex(['Basic','Silver','Gold']).reset_index()
 
-    items = [('Gender Distribution (%)', g, ['Gender', 'Percentage (%)']),('Average Spending per Loyalty Tier', t, ['Tier', 'Avg Spending ($)'])]
+    data = [('Gender Distribution (%)', g, ['Gender', 'Percentage (%)']),('Average Spending per Loyalty Tier', t, ['Tier', 'Avg Spending ($)'])]
     
-    for t, df, c in items:
+    for t, df, c in data:
         fig, ax = plt.subplots(figsize=(6,2))
         ax.axis('off'); ax.set_title(t,weight='bold'); ax.table(cellText=df.values, colLabels=c, loc='center', cellLoc='center').scale(1,2)
         pdf.savefig(fig, bbox_inches='tight'); plt.close()
