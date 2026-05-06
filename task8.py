99fda7e286dd1c91c5651d6d88364ca1312926e6
thanachai4271-ux <thanachai4271@gmail.com>
2026-05-06 15:47:33 +0700

    Update task8.py

--- task8.py
+++ task8.py
@@ -4,7 +4,7 @@ s, p = pd.read_csv('sales_transactions_cleaned.csv'), pd.read_csv('products.csv'
 s['r'] = s.quantity * s.price - s.discount_amount.fillna(0)
 p['c'] = pd.to_numeric(p.cost.astype(str).str.replace(r'[^0-9./-]', '', regex=1), 'coerce')
 
-f = s.groupby('product_id', as_index=0).agg(total_quantity_sold=('quantity','sum'), total_revenue=('r','sum')).merge(p[['product_id','c']])
+f = s.groupby('product_id', as_index=0).agg(tqs=('quantity','sum'), tr=('r','sum')).merge(p[['product_id','c']])
 f['profit_margin'] = (1 - f.total_quantity_sold * f.c / f.total_revenue).round(4)
 f.drop(columns='c').sort_values('total_revenue', ascending=0).round(2).to_csv('Session5_Product_Performance_short_short.csv', index=0)
 
